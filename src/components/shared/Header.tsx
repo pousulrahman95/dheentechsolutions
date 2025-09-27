d7985708591b85afd7aebc37c359094b53d8dc23
pousulrahman95 <pousulrahman95@gmail.com>
2025-09-27 22:16:18 +0530

    Updated Header.tsx

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -34,7 +34,7 @@ const Header = () => {
             <img 
               src="/Untitled design (2).png" 
               alt="Dheen Tech Solution" 
-              className="h-10 w-10"
+              className="h-5 w-auto"
             />
             <span className="text-xl font-bold text-gray-900">Dheen Tech Solutions</span>
           </Link>
