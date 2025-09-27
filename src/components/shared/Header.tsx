7cb4355db838cceb32b85347cf484d3da09035ed
pousulrahman95 <pousulrahman95@gmail.com>
2025-09-27 22:08:18 +0530

    Updated Header.tsx

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -34,7 +34,7 @@ const Header = () => {
             <img 
               src="/Untitled design (2).png" 
               alt="DTS SOFTWARE" 
-              className="h-10 w-5"
+              className="h-10 w-10"
             />
             <span className="text-xl font-bold text-gray-900">Dheen Tech Solutions</span>
           </Link>
