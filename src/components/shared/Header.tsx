a4ce899b53c1afef14344f300a7151088a544b56
pousulrahman95 <pousulrahman95@gmail.com>
2025-09-27 20:26:11 +0530

    Updated Header.tsx

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -40,7 +40,7 @@ const Header = () => {
           </Link>
 
           {/* Desktop Navigation */}
-           <nav className="mr-80"> 
+           <nav className="mr-70"> 
         <ul className="flex space-x-16">
           {navigation.map((item) => (
             <li key={item.name}>
